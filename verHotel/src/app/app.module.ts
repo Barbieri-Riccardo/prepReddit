d4ac88d01b26fd44a0d3a43d192f2cebd3abe19f
Barbieri-Riccardo <barbieri.riccardo@iisgalvanimi.edu.it>
2022-05-01 15:41:44 +0000

    ho capito

---
+++ verHotel/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { ListaprenotazioniComponent } from './listaprenotazioni/listaprenotazioni.component';
+import { DettagliStanzaComponent } from './dettagli-stanza/dettagli-stanza.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListaprenotazioniComponent,
+    DettagliStanzaComponent,
+  ],
+  imports: [
+    BrowserModule, HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ verHotel/src/app/listaprenotazioni/listaprenotazioni.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Booking } from '../models/booking.model';
+import { Room } from '../models/room.model';
+
+@Component({
+  selector: 'app-listaprenotazioni',
+  templateUrl: './listaprenotazioni.component.html',
+  styleUrls: ['./listaprenotazioni.component.css']
+})
+export class ListaprenotazioniComponent implements OnInit {
+
+  constructor() { }
+  @Input() listaPreno : Booking[] = undefined!;
+  room : Room = undefined!
+  ngOnInit(): void {
+  }
+  mostraDettagli(room:Room){
+    this.room = room
+  }
+}
